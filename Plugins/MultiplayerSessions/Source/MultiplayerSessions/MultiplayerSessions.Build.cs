82d288d532783010f08c7d14dad5571357274196
B0 <1149337319@qq.com>
2026-06-10 21:38:05 +0800

    Add: Menu

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
@@ -29,6 +29,9 @@ public class MultiplayerSessions : ModuleRules
 				"Core",
 				"OnlineSubsystem",
 				"OnlineSubsystemSteam",
+				"UMG",
+				"Slate",
+				"SlateCore"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
